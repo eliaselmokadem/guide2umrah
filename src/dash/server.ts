c96e88ee0dafb721d3f4dc77c6774b4c76345386
eliaselmokadem <s141765@ap.be>
2024-11-22 21:32:44 +0100

    aws fix

--- src/dash/server.ts
+++ src/dash/server.ts
@@ -10,10 +10,12 @@ app.use(express.json());
 app.use(cors());
 
 // Verbinding maken met MongoDB
+const mongoURI =
+  process.env.MONGO_URI ||
+  "mongodb+srv://admin:admin@g2mdb.spmzf.mongodb.net/?retryWrites=true&w=majority&appName=g2mdb";
+
 mongoose
-  .connect(
-    "mongodb+srv://admin:admin@g2mdb.spmzf.mongodb.net/?retryWrites=true&w=majority&appName=g2mdb"
-  )
+  .connect(mongoURI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
@@ -43,9 +45,13 @@ app.post("/login", async (req: Request, res: Response) => {
     }
 
     // Maak een JWT-token aan
-    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET || "your-secret-key",
+      {
+        expiresIn: "1h",
+      }
+    );
     res.json({ token });
   } catch (err) {
     console.error(err);
@@ -62,6 +68,7 @@ app.get("*", (req: Request, res: Response) => {
 });
 
 // Server starten
-app.listen(5000, () => {
-  console.log("Server started on http://localhost:5000");
+const PORT = process.env.PORT || 5000; // Gebruik dynamische poort als die beschikbaar is
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });

18c4eaf247cdb2d5fdba682da3bc1a78dcf3cead
eliaselmokadem <s141765@ap.be>
2025-01-01 20:22:21 +0100

    api test

--- api/server.ts
+++ api/server.ts
@@ -44,15 +44,12 @@ cloudinary.config({
 
 // Email configuration
 const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
-  },
-  tls: {
-    ciphers: 'SSLv3'
   }
 });
 
